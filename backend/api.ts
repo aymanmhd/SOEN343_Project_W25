50c74526aa06e0196cf211aecc5f17e50f6c9d4e
Speciaa <parsan1387@gmail.com>
2025-03-27 02:36:28 -0400

    Implemented login feature with backend

--- backend/api.ts
+++ backend/api.ts
@@ -34,8 +34,8 @@ mongoose.model('Mail', MailSchema);
 mongoose.model('Order', OrderSchema);
 
 const apiServer = express();
-const port = 3000;
-const websiteUrl = "http://localhost";
+const port = 8888;
+const websiteUrl = "http://localhost:3000";
 
 setTimeout(async () => {
     database.dropDatabase();
@@ -86,7 +86,7 @@ setTimeout(async () => {
 }, 2000);
 
 var corsOptions = {
-    origin: "",
+    origin: websiteUrl,
     credentials: true,
     optionsSuccessStatus: 200
 };
@@ -135,7 +135,8 @@ apiServer.post("/login", async (req, res) => {
     LoginStreak.updateLoginStreak(account);  // Update login streak asynchronously
 
     // res.status(200).send("Logged In!");
-    return res.redirect(websiteUrl + "/account.html");
+    console.log("Successfully logged in!");
+    return res.redirect(websiteUrl + "/account");
 });
 
 apiServer.post("/register", async (req, res) => {
